be6cb27e09d88e432fbdf4776786f5d756f3d56b
Vladimir Starkov <iamstarkov@gmail.com>
2015-07-24 07:49:34 +0500

    add tests

--- test.js
+++ test.js
@@ -1,6 +1,20 @@
 import { equal } from 'assert';
-import getMdDesc from './index';
+import getDesc from './index';
 
-it('should getMdDesc', () => {
-  equal(getMdDesc('unicorns'), 'unicorns');
+const input = `
+# title
+
+Published yesterday
+
+Or in 21 december 2012
+
+True *description*
+`.trim();
+
+it('should getDesc text', () => {
+  equal(getDesc(/Published|december/, input).text, 'True description');
+});
+
+it('should getDesc html', () => {
+  equal(getDesc(/Published|december/, input).html, 'True <em>description</em>');
 });
