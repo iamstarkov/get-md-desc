ac1e62fc49f1a21f9c208428f0195945021e84c1
Vladimir Starkov <iamstarkov@gmail.com>
2015-07-24 07:49:55 +0500

    add implementation

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-export default item => item;
+import { text, html, match, isParagraph } from 'commonmark-helpers';
+import trimTag from 'trim-html-tag';
+
+const isDesc = (exclude, node) =>
+  isParagraph(node) && !text(node).match(exclude);
+
+const result = node => ({
+  text: text(node),
+  html: trimTag('p', html(node)),
+  node
+});
+
+export default (exclude, input) =>
+  result(match(input, node => isDesc(exclude, node)));
