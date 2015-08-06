fe3dce80eaed0f47f68af2099d4351879f0c583b
Vladimir Starkov <iamstarkov@gmail.com>
2015-08-06 15:46:10 +0200

    small refactoring

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import { text, html, match, isParagraph } from 'commonmark-helpers';
 import trimTag from 'trim-html-tag';
 
-const isDesc = (exclude, node) =>
+const isDesc = (node, exclude) =>
   isParagraph(node) && !text(node).match(exclude);
 
 export default (input, exclude = null) => {
-  let node = match(input, node => isDesc(exclude, node));
+  let node = match(input, node => isDesc(node, exclude));
   if (!node) return;
   return {
     text: text(node),
