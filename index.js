fd78a031b62699628cd16eb69e930c61c48efc69
Vladimir Starkov <iamstarkov@gmail.com>
2015-08-29 14:59:35 +0200

    implement the fix for new testcases

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import { text, html, match, isParagraph } from 'commonmark-helpers';
+import { text, html, match, isParagraph, isRoot } from 'commonmark-helpers';
 import trimTag from 'trim-html-tag';
 
 const isDesc = (node, exclude) =>
-  isParagraph(node) && !text(node).match(exclude);
+  isRoot(node) && isParagraph(node) && !text(node).match(exclude);
 
 export default (input, exclude = null) => {
   let node = match(input, node => isDesc(node, exclude));
